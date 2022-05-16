ebdea7b16967c98105f66d635e067e8dd5ea05a5
DarFelix <xbxb78@gmail.com>
2022-05-16 12:29:57 -0500

    creacion proyecto

---
+++ api-citas/src/main/java/com/iudigital/ApiCitasApplication.java
@@ -0,0 +1,13 @@
+package com.iudigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCitasApplication.class, args);
+	}
+
+}
---
+++ api-citas/src/test/java/com/iudigital/ApiCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iudigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
